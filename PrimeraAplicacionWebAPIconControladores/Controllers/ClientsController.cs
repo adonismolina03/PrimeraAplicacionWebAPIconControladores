870e89a0406a3973381bb72b5d4cef891d7e9917
adonismolina03 <adonismolina03@gmail.com>
2023-09-05 17:28:56 -0600

    guia terminada

---
+++ PrimeraAplicacionWebAPIconControladores/Controllers/ClientsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeraAplicacionWebAPIconControladores.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PrimeraAplicacionWebAPIconControladores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        static List<Client> clients = new List<Client>();
+
+        // GET: api/<ClientsController>
+        [HttpGet]
+        public IEnumerable<Client> Get()
+        {
+            return clients;
+        }
+
+        // GET api/<ClientsController>/5
+        [HttpGet("{id}")]
+        public Client Get(int id)
+        {
+            var client = clients.FirstOrDefault(x => x.Id == id);
+            return client;
+        }
+
+        // POST api/<ClientsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Client client)
+        {
+            clients.Add(client);
+            return Ok();
+        }
+
+        // PUT api/<ClientsController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Client client)
+        {
+            var existingClient = clients.FirstOrDefault(c => c.Id == id);
+            if (existingClient != null)
+            {
+                existingClient.Name = client.Name;
+                existingClient.LastName = client.LastName;
+                return Ok();
+            }
+            else 
+            {
+                return NotFound();
+            }
+        }
+
+        // DELETE api/<ClientsController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existingClient = clients.FirstOrDefault(c => c.Id == id);
+            if (existingClient != null)
+            {
+                clients.Remove(existingClient);
+                return Ok();
+            }
+            else 
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ PrimeraAplicacionWebAPIconControladores/Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimeraAplicacionWebAPIconControladores.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
